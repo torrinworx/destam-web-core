7454648dedecfcc2f7e7e8dac5d13a0c4bcd23c0
torrin1 <torrin@worx4you.com>
2024-11-08 09:39:54 -0500

    adding form for frontend signup stuff

--- example/index.jsx
+++ example/index.jsx
@@ -3,15 +3,11 @@ import { coreClient, jobRequest } from '../client/coreClient.jsx';
 import { Typography, TextField, Button } from 'destamatic-ui';
 
 // Setting the session token:
-const expires = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toUTCString();
-const sessionToken = 'asasdasd';
-document.cookie = `webCore=${sessionToken}; expires=${expires}; path=/; SameSite=Lax`;
 
 const App = ({ state }) => {
     // const email = state.client.observer.path('email').def('');
-	const email = Observer.mutable();
-    console.log(email);
-    console.log(TextField);
+    const email = Observer.mutable('');
+    const password = Observer.mutable('');
 
     return <div>
         <Button
@@ -30,14 +26,36 @@ const App = ({ state }) => {
                 console.log(test);
             }}
         />
-		<TextField value={email} placeholder="Email" />
+        <br />
+        <div style={{ display: 'flex', flexDirection: 'column', width: '200px' }}>
+            <TextField value={email} placeholder="Email" />
+            <TextField value={password} placeholder="Password" />
+        </div>
+        <Button
+            type='contained'
+            label='Signup'
+            onMouseDown={async () => {
+                const response = await jobRequest('signup', { email: email.get(), password: password.get() });
+                console.log(response);
+            }}
+        />
+        <Button
+            type='contained'
+            label='Login'
+            onMouseDown={async () => {
+                const response = await jobRequest('login', { email: email.get(), password: password.get() });
+                console.log(response);
 
+                const expires = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toUTCString();
+                const sessionToken = response.result.sessionToken;
+                document.cookie = `webCore=${sessionToken}; expires=${expires}; path=/; SameSite=Lax`;
+            }}
+        />
     </div>;
 };
 
 const NotFound = () => <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
     <Typography type='h4'>Not Found 404</Typography>
 </div>;
-
 
 coreClient(App, NotFound);
