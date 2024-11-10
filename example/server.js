b48bef02ae7d452a16aa70922046bccd84084d89
torrin1 <torrin@worx4you.com>
2024-11-10 10:20:31 -0500

    Adding a user check

--- example/server.js
+++ example/server.js
@@ -8,11 +8,13 @@ const connection = async (ws, req) => {
     const sessionToken = new URLSearchParams(req.url.split('?')[1]).get('sessionToken');
 
     const user = await ODB('users', { "sessions": sessionToken });
-    let sync = await ODB('state', { userID: user.userID });
-    if (!sync) {
-        sync = await ODB('state', {}, OObject({
-            userID: user.userID,
-        }));
+    if (user) {
+        let sync = await ODB('state', { userID: user.userID });
+        if (!sync) {
+            sync = await ODB('state', {}, OObject({
+                userID: user.userID,
+            }));
+        }
     }
 
     return {
