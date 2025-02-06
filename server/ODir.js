51c66820094ab7ff989e4a1625629d6597ed8f5b
torrin1 <torrin@worx4you.com>
2025-02-05 19:53:27 -0500

    Adding notes from Alex for future upgrade

--- server/ODir.js
+++ server/ODir.js
@@ -22,6 +22,101 @@ const getDirectoryState = (dirPath) => {
 	}, {});
 };
 
+/**
+ * One comment: Observables are not supposed to implement a watch function. Instead, they're supposed to call into the network with Network.linkApply
+ * The network is the thing that implements the fine grained listeners that we have
+ * so .path .ignore .skip ... etc
+ * But I don't know if the ideas are really all that compatible: Observables are synchronious while the fs is asynchronious. That's why you have to put the 'sync' versions everywhere
+ * - Alex
+ * 
+ * 	
+alex
+5:37 PM
+One comment: Observables are not supposed to implement a watch function. Instead, they're supposed to call into the network with Network.linkApply
+
+The network is the thing that implements the fine grained listeners that we have
+
+so .path .ignore .skip ... etc
+
+
+torrin
+5:38 PM
+Oooooh I see
+
+
+alex
+5:38 PM
+Not a bad idea
+
+But I don't want to implement nodejs specific stuff in destam because that's supposed to be backend/frontend
+
+As a seperate library maybe 'destam-fs' would work
+
+
+torrin
+5:40 PM
+Ah right right, makes sense, well thanks! I came up with it because I wanted to watch a dir with images for my personal site 
+
+
+alex
+5:40 PM
+But I don't know if the ideas are really all that compatible: Observables are synchronious while the fs is asynchronious. That's why you have to put the 'sync' versions everywhere
+
+Plus it won't work if you try to send a ODir or OFile over the network to the frontend or something
+
+I would suggest an alternate approach that doesn't require building a new observable:
+
+
+torrin
+5:44 PM
+Oh I see, so maybe just a really fancy OArray?
+
+
+alex
+5:46 PM
+Build a state tree but modify it from the outside:
+
+const createObservableFromFS = (fsFile) => {
+const files = OObject();
+
+for (const file in fsFile.getFiles()) {
+if (file.isDirectory()){
+   files[file.name] = createObservableFromFS(file);
+} else {
+   files[file.name] = {name: file.name, contents: ...};
+}
+}
+
+fsFile.watch(() => {
+ // update the files oobject
+});
+}
+
+torrin
+5:48 PM
+Ah ok and fsFile is the existing ODir .getFiles() method using fs? 
+
+
+alex
+5:49 PM
+Yeah, it's pseudo code
+
+
+torrin
+5:49 PM
+oh right right mb
+
+5:49 PM
+
+
+
+
+
+
+
+ok cool that makes sense
+ */
+
 const ODir = createClass((dirPath, id) => {
 	assert(typeof dirPath === 'string', 'Directory path must be a string');
 
