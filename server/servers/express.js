ae259d49c3b9c0042bbc476afe34f3eda7515060
torrin1 <torrin@worx4you.com>
2025-03-13 18:35:18 -0400

    Adding server drivers

---
+++ server/servers/express.js
@@ -0,0 +1,45 @@
+import fs from 'fs';
+import express from 'express';
+
+// express server driver:
+export default () => {
+	const app = express();
+
+	return {
+		production: ({ root }) => {
+			app.use(express.static(root));
+
+			app.get('*', (req, res) => {
+				res.sendFile('index.html', { root }, err => {
+					if (err) {
+						res.status(500).send(err);
+						console.error('Error serving index.html:', err);
+					}
+				});
+			});
+		},
+		development: ({ vite }) => {
+			app.use(vite.middlewares);
+
+			app.get('*', async (req, res, next) => {
+				try {
+					const html = await vite.transformIndexHtml(
+						req.originalUrl,
+						fs.readFileSync(
+							path.resolve(root, 'index.html'),
+							'utf-8'
+						)
+					);
+
+					res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
+				} catch (e) {
+					vite.ssrFixStacktrace(e);
+					next(e);
+				}
+			});
+		},
+		listen: () => app.listen(process.env.PORT || 3000, () => {
+			console.log(`Express is serving on http://localhost:${process.env.PORT || 3000}/`);
+		})
+	}
+};
