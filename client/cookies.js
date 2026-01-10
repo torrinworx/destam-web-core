b3d10bbb8bf498318e8dbb0467b38b7d4919bca4
Torrin Leonard <torrin@torrin.me>
2026-01-10 17:04:15 -0500

    Simplifying cookies

--- client/cookies.js
+++ client/cookies.js
@@ -1,67 +1,30 @@
-// stolen from: https://stackoverflow.com/a/63952971/15739035
-// Listen for when web-core cookie is changed, re-init state if webcore cookie deleted (only deleted on signout)
-// document.addEventListener('cookiechange', async ({ detail: { newValue, oldValue } }) => {
-// });
-export const cookieUpdates = () => {
-	const parseCookieString = (cookieString) => {
-		const result = {};
-		cookieString.split('; ').forEach(cookie => {
-			const [key, value] = cookie.split('=');
-			result[key] = value;
-		});
-		return result;
-	};
+import { Observer } from 'destamatic-ui';
 
-	const areCookiesEqual = (cookieA, cookieB) => {
-		// Check that all keys and values are the same
-		if (Object.keys(cookieA).length !== Object.keys(cookieB).length) return false;
-		for (const key in cookieA) {
-			if (cookieA[key] !== cookieB[key]) {
-				return false;
-			}
-		}
-		return true;
-	};
+// snapshot once on module load
+const initialToken = (() => {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; webcore=`);
+  if (parts.length === 2) return parts.pop().split(';').shift() || '';
+  return '';
+})();
 
-	let lastCookie = parseCookieString(document.cookie);
-	const expando = '_cookie';
-	let nativeCookieDesc = Object.getOwnPropertyDescriptor(Document.prototype, 'cookie');
+export const webcoreToken = Observer.mutable(initialToken);
 
-	Object.defineProperty(Document.prototype, expando, nativeCookieDesc);
-	Object.defineProperty(Document.prototype, 'cookie', {
-		enumerable: true,
-		configurable: true,
-		get() {
-			return this[expando];
-		},
-		set(value) {
-			this[expando] = value;
-			let cookie = parseCookieString(this[expando]);
-			if (!areCookiesEqual(cookie, lastCookie)) {
-				try {
-					let detail = { oldValue: lastCookie, newValue: cookie };
-					this.dispatchEvent(new CustomEvent('cookiechange', {
-						detail: detail
-					}));
-					channel.postMessage(detail);
-				} finally {
-					lastCookie = cookie;
-				}
-			}
-		}
-	});
+export const setWebcoreToken = (token) => {
+  webcoreToken.set(token || '');
+  if (!token) return;
 
-	const channel = new BroadcastChannel('cookie-channel');
-	channel.onmessage = (e) => {
-		lastCookie = e.data.newValue;
-		document.dispatchEvent(new CustomEvent('cookiechange', {
-			detail: e.data
-		}));
-	};
+  const expires = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toUTCString();
+  document.cookie = `webcore=${token}; expires=${expires}; path=/; SameSite=Lax`;
+};
+
+export const clearWebcoreToken = () => {
+  webcoreToken.set('');
+  document.cookie = 'webcore=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=Lax';
 };
 
 export const getCookie = (name) => {
-	const value = `; ${document.cookie}`;
-	const parts = value.split(`; ${name}=`);
-	if (parts.length === 2) return parts.pop().split(';').shift();
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
 };
