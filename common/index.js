715532cc8adf656f60f0d466eaf6a48043412372
Torrin Leonard <torrin@torrin.me>
2026-02-09 15:55:46 -0500

    removing junk

--- common/index.js
+++ common/index.js
@@ -1,4 +1,3 @@
 export { parse, clone, stringify } from './clone.js';
 export { asyncSwitch } from './asyncSwitch.js';
 export { default as Obridge } from './Obridge.js';
-export { default as paginate } from './paginate.js';
