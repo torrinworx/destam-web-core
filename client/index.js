3f0d7e5632063a635954491abf7a7e077a1af08b
Torrin Leonard <torrin@torrin.me>
2026-02-27 09:22:58 -0500

    Stasis: Adding new stasis scomponent

--- client/index.js
+++ client/index.js
@@ -2,3 +2,4 @@ export * from './core.jsx';
 export * from './cookies.js';
 
 export { default as ActionField } from './components/ActionField/ActionField.jsx';
+export { default as Stasis } from './components/Stasis/Stasis.jsx';
